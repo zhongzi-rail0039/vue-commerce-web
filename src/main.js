f34e2e5eb67e922331258866826852ee9f4a6698
粽子 <rail_0039@126.com>
2020-02-23 10:45:55 +0800

    组件-静态编写

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import router from './router.js'
+import './assets/css/index.css'
+import './assets/css/base.css'
 
 Vue.use(VueAxios, axios)
 // 提升开关
